bd8b5463afc75a63610a300f923ee5ab6c45e1af
mattjkatz <matthewkatz7@gmail.com>
2022-03-16 20:20:31 -0500

    create index and create actions and add validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"]
+    )
+    if user.save
+      render json: user
+    else
+      render json: {error_message: user.errors.full_messages}, status: 422
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  validates :first_name, presence: true, uniqueness: true
+  validates :last_name, presence: true, uniqueness: true
+
 end
